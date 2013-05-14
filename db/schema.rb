e76ddd4961e3a7260912f8b07f90afd116593878
Michael Schiesari <michael.schiesari@gmail.com>
2013-05-14 07:24:31 +0100

    before modeling users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130504153005) do
+ActiveRecord::Schema.define(:version => 20130510124843) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(:version => 20130504153005) do
     t.datetime "updated_at",   :null => false
     t.string   "excerpt"
     t.string   "location"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
